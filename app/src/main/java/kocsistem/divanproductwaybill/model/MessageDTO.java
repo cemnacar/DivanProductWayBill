0554a9d367854e45c58896a74db8cae652fe334c
cemnacar cem.nacar@hotmail.com <cem.nacar@hotmail.com>
2015-11-02 14:01:23 +0300

    ttt

--- app/src/main/java/kocsistem/divanproductwaybill/model/MessageDTO.java
+++ app/src/main/java/kocsistem/divanproductwaybill/model/MessageDTO.java
@@ -4,6 +4,23 @@ package kocsistem.divanproductwaybill.model;
  * Created by 02482957 on 2.11.2015.
  */
 public class MessageDTO {
-    public String Type;
-    public String Message;
+    public void setTipField(String tipField) {
+        this.tipField = tipField;
+    }
+
+    public void setMesajField(String mesajField) {
+        this.mesajField = mesajField;
+    }
+
+    public String getMesajField() {
+
+        return mesajField;
+    }
+
+    public String getTipField() {
+        return tipField;
+    }
+
+    public String tipField;
+    public String mesajField;
 }
